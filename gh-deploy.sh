f6cdc9317634c873b9449e7d05b95b64f92e0190
Simon Brigant <simon.brigant@open-groupe.com>
2020-01-28 09:35:27 +0100

    final

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
+
 if [ -z "$1" ]
 then
   echo "You need to enter a commit message"
   exit 1
 fi
 
-git add dist && git commit -m "$1"
-git subtree push --prefix dist origin gh-pages
+npx gulp dist --codelabs-dir=codelabs
+
+rm -rf .firebase
+cp -r dist/codelabs dist/codelabs_tmp
+rm -r dist/codelabs
+cp -r dist/codelabs_tmp dist/codelabs
+rm -r dist/codelabs_tmp
+
+
+git add -A && git commit -m "$1"
+npm version major
+git push
+# git subtree push --prefix dist origin gh-pages
+
+firebase deploy
